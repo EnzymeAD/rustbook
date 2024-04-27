ea15b666100724908e67f4e1b7196627b0f802bb
Manuel Drehwald <git@manuel.drehwald.info>
2024-04-27 00:50:04 -0400

    remove not yet handled tests

--- ui/tests/ui/wrong_activity2.rs
+++
@@ -1,6 +0,0 @@
-#![feature(autodiff)]
-
-#[autodiff(d_f2, Reverse, Duplicated, Active)]
-fn f2(x: f64) -> f64 {
-    x * x
-}
--- ui/tests/ui/wrong_activity3.rs
+++
@@ -1,6 +0,0 @@
-#![feature(autodiff)]
-
-#[autodiff(d_f3, Reverse, Active, Active)]
-fn f3(x: i64) -> f64 {
-    (x * x) as f64
-}
